487c9db84138cc60ec0d5f731c1e7996ab478db4
Sagar <sagar.arora198@gmail.com>
2024-02-22 10:02:23 +0530

    Responsive

--- index.js
+++ index.js
@@ -1,112 +1,145 @@
-
 // Function to handle the send button click
 async function handleSend() {
     const inputElement = document.querySelector('.inputUrl').value;
     // console.log(inputElement.value);
 
     // call fetch api
-    const response = await fetch(inputElement,{
-        method:"GET",
+    const response = await fetch(inputElement, {
+        method: "GET",
         mode: "cors"
     })
     const data = await response.json()
     let objectdata = data[0]
     console.log(data);
     let responseoutput = document.querySelector('.response')
+    responseoutput.value = ''
+    responseoutput.value = '{\n'
     for (const i in objectdata) {
         if (Object.hasOwnProperty.call(objectdata, i)) {
             const element = objectdata[i];
             responseoutput.value += `${i} : ${element} \n`
-            
+
         }
     }
-    
+    responseoutput.value += '\n}'
+
 }
-let previousKeyInput = ''; // Variable to store the previous value of .key
+// let previousKeyInput = ''; // Variable to store the previous value of .key
 
-function handelInput() {
-    const inputKeyElement = document.querySelector('.inp');
-    const urlText = document.querySelector('.inputUrl');
-    
-    // Get the current input value
-    const currentInputValue = urlText.value;
-    
-    
+// function handelInput() {
+//     const inputKeyElement = document.querySelector('.inp');
+//     const urlText = document.querySelector('.inputUrl');
 
-    // Calculate the difference between the new input and the previous input
-    const diff = getDifference(inputKeyElement.value, previousKeyInput);
+//     // Get the current input value
+//     const currentInputValue = urlText.value;
 
-    // If the input is a deletion, remove characters from .inputUrl
-    if (diff.startsWith('-')) {
-        // Remove the deleted characters from .inputUrl
-        const deletedChars = diff.slice(1); // Exclude the '-' indicating deletion
-        const lastIndexOfDeletedChars = currentInputValue.lastIndexOf(deletedChars);
-        
-        if (lastIndexOfDeletedChars !== -1) {
-            urlText.value = currentInputValue.substring(0, lastIndexOfDeletedChars);
+
+
+//     // Calculate the difference between the new input and the previous input
+//     const diff = getDifference(inputKeyElement.value, previousKeyInput);
+
+//     // If the input is a deletion, remove characters from .inputUrl
+//     if (diff.startsWith('-')) {
+//         // Remove the deleted characters from .inputUrl
+//         const deletedChars = diff.slice(1); // Exclude the '-' indicating deletion
+//         const lastIndexOfDeletedChars = currentInputValue.lastIndexOf(deletedChars);
+
+//         if (lastIndexOfDeletedChars !== -1) {
+//             urlText.value = currentInputValue.substring(0, lastIndexOfDeletedChars);
+//         }
+//     } else {
+//         // Append .inputUrl before the new characters from .key
+//         urlText.value = currentInputValue + diff;
+//     }
+
+//     // Update the previousKeyInput for the next comparison
+//     previousKeyInput = inputKeyElement.value;
+// }
+
+// // Function to calculate the difference between two strings
+// function getDifference(newStr, oldStr) {
+//     let diff = '';
+
+//     for (let i = 0; i < newStr.length || i < oldStr.length; i++) {
+//         if (newStr[i] !== oldStr[i]) {
+//             if (i < oldStr.length) {
+//                 diff += '-' + oldStr[i]; // Indicates deletion and append the deleted character
+//             } else {
+//                 diff += newStr[i];
+//             }
+//         }
+//     }
+
+//     return diff;
+// }
+
+// let previousValueInput=''
+// function handelValueInput(){
+//     const inputKeyElement = document.querySelector('.value');
+//     const urlText = document.querySelector('.inputUrl');
+
+//     // Get the current input value
+//     const currentInputValue = urlText.value;
+
+
+
+//     // Calculate the difference between the new input and the previous input
+//     const diff = getDifference(inputKeyElement.value, previousKeyInput);
+
+//     // If the input is a deletion, remove characters from .inputUrl
+//     if (diff.startsWith('-')) {
+//         // Remove the deleted characters from .inputUrl
+//         const deletedChars = diff.slice(1); // Exclude the '-' indicating deletion
+//         const lastIndexOfDeletedChars = currentInputValue.lastIndexOf(deletedChars);
+
+//         if (lastIndexOfDeletedChars !== -1) {
+//             urlText.value = currentInputValue.substring(0, lastIndexOfDeletedChars);
+//         }
+//     } else {
+//         // Append .inputUrl before the new characters from .key
+//         urlText.value = currentInputValue + diff;
+//     }
+
+//     // Update the previousKeyInput for the next comparison
+//     previousKeyInput = inputKeyElement.value;
+// }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const inputUrl = document.getElementById('urlInput')
+    const queryInput = document.getElementById('inp')
+    queryInput.addEventListener('input', function () {
+        const queryParams = this.value.trim()
+        if (inputUrl.value.includes('?')) {
+            const urlWithoutParams = inputUrl.value.split('?')[0]
+            inputUrl.value = urlWithoutParams + '?' + queryParams
+
         }
-    } else {
-        // Append .inputUrl before the new characters from .key
-        urlText.value = currentInputValue + diff;
-    }
-
-    // Update the previousKeyInput for the next comparison
-    previousKeyInput = inputKeyElement.value;
-}
-
-// Function to calculate the difference between two strings
-function getDifference(newStr, oldStr) {
-    let diff = '';
-
-    for (let i = 0; i < newStr.length || i < oldStr.length; i++) {
-        if (newStr[i] !== oldStr[i]) {
-            if (i < oldStr.length) {
-                diff += '-' + oldStr[i]; // Indicates deletion and append the deleted character
-            } else {
-                diff += newStr[i];
-            }
+        else {
+            inputUrl.value = inputUrl.value.trim() + '?' + queryParams
         }
-    }
+    })
+})
+document.addEventListener('DOMContentLoaded', function () {
+    const inputUrl = document.getElementById('urlInput')
+    const queryInput = document.getElementById('valueinp')
+    queryInput.addEventListener('input', function () {
+        const queryParams = this.value.trim()
+        if (inputUrl.value.includes('=')) {
+            const urlWithoutParams = inputUrl.value.split('=')[0]
+            inputUrl.value = urlWithoutParams + '=' + queryParams
 
-    return diff;
-}
-
-let previousValueInput=''
-function handelValueInput(){
-    const inputKeyElement = document.querySelector('.value');
-    const urlText = document.querySelector('.inputUrl');
-    
-    // Get the current input value
-    const currentInputValue = urlText.value;
-    
-    
-
-    // Calculate the difference between the new input and the previous input
-    const diff = getDifference(inputKeyElement.value, previousKeyInput);
-
-    // If the input is a deletion, remove characters from .inputUrl
-    if (diff.startsWith('-')) {
-        // Remove the deleted characters from .inputUrl
-        const deletedChars = diff.slice(1); // Exclude the '-' indicating deletion
-        const lastIndexOfDeletedChars = currentInputValue.lastIndexOf(deletedChars);
-        
-        if (lastIndexOfDeletedChars !== -1) {
-            urlText.value = currentInputValue.substring(0, lastIndexOfDeletedChars);
         }
-    } else {
-        // Append .inputUrl before the new characters from .key
-        urlText.value = currentInputValue + diff;
-    }
+        else {
+            inputUrl.value = inputUrl.value.trim() + '=' + queryParams
+        }
+    })
+})
 
-    // Update the previousKeyInput for the next comparison
-    previousKeyInput = inputKeyElement.value;
-}
-
-function addQuestionMark(){
+function addQuestionMark() {
     const inputdata = document.querySelector('.inputUrl')
-    inputdata.value = inputdata.value+'?'
+    inputdata.value = inputdata.value + '?'
 }
-function addEqualMark(){
+function addEqualMark() {
     const inputdata = document.querySelector('.inputUrl')
-    inputdata.value = inputdata.value+'='
+    inputdata.value = inputdata.value + '='
 }
